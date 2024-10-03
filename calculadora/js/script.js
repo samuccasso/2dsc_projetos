f20837ee9aa3295af6aa52067ed838bae7053d61
samuccasso <samuelramoscarvalhoo@gmail.com>
2024-10-03 15:24:49 +0000

    iniciando o código Javascript

---
+++ calculadora/js/script.js
@@ -0,0 +1,19 @@
+// FAZ REFERÊNCIA AOS ELEMENTOS DOM
+// alert("NÃO SABE MONTAR NEM UMA EQUAÇÃO?? imbecil inútil...")
+const numero1 = document.querySelector('#numero1')
+const numero2 = document.querySelector('#numero2')
+const btnCalcular = document.querySelector('#btn-calcular')
+const btnCalcular = document.querySelector('#btn-calcular')
+const btnCalcular = document.querySelector('#btn-calcular')
+const btnCalcular = document.querySelector('#btn-calcular')
+const btnCalcular = document.querySelector('#btn-calcular')
+const btnCalcular = document.querySelector('#btn-calcular')
+const btnCalcular = document.querySelector('#btn-calcular')
+alert('numero1')
+
+
+// CRIA A FUNÇÃO CALCULAR
+
+
+// ADICIONA ESCUTAR DE EVENTOS
+
